6562d77f793ebb29fb99d56215dfd993ce7b3302
Luis Rodriguez <luisrodge@outlook.com>
2020-07-26 13:09:03 -0600

    Install and setup typeorm, create initial user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { configService } from './config/config.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    UsersModule,
+  ],
 })
 export class AppModule {}
