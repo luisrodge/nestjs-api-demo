484dd701b4b16dddc5161aa749343bb4cde48864
Luis Rodriguez <luisrodge@outlook.com>
2020-07-26 17:25:47 -0600

    Enable cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { configService } from './config/config.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './core/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    AuthModule,
     UsersModule,
   ],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserEntity } from './user.entity';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
+  exports: [UsersService],
+  providers: [UsersService],
 })
 export class UsersModule {}
