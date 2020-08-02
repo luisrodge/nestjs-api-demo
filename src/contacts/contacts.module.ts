acaeb19cc6226d8b859ecc12ed233c85917864df
Luis Rodriguez <luisrodge@outlook.com>
2020-08-02 14:31:34 -0600

    Update contacts module

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ContactEntity } from './contact.entity';
+import { ContactsService } from './contacts.service';
+import { ContactsController } from './contacts.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContactEntity])],
+  controllers: [ContactsController],
+  exports: [ContactsService],
+  providers: [ContactsService],
 })
 export class ContactsModule {}
